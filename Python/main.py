9563cca48913da6e1905ae06a86f00352f398437
kwonsss <sh.kwon@dhandy.co.kr>
2024-09-03 18:09:29 +0900

    Python Thread (Not work)

---
+++ Python/main.py
@@ -0,0 +1,57 @@
+import pika
+
+class RpcClient(object):
+
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+        self.response = None
+        self.corr_id = None
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(n)
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='rpc_queue',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body=str(n))
+        while self.response is None:
+            self.connection.process_data_events(time_limit=None)
+        return (self.response)
+
+
+# Rabbit
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.basic_publish(exchange='', routing_key='Producer', body='Hello World!')
+connection.close()
+
+
+rpc_client = RpcClient()
+
+print(" [x] Requesting fib(30)")
+response = rpc_client.call(30)
+print(f" [.] Got {response}")
+
+# Python Thread Not Work
+
+print("End")
