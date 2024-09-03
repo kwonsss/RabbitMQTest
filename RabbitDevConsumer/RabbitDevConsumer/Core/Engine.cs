b1ecf03a6ecc1012117770869c39c521c1b19f23
kwonsss <sh.kwon@dhandy.co.kr>
2024-09-03 14:20:22 +0900

    Hello World

---
+++ RabbitDevConsumer/RabbitDevConsumer/Core/Engine.cs
@@ -0,0 +1,62 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitDevTool.Core
+{
+    public class Engine
+    {
+
+        public Action<string> Output { get; set; }
+
+        private IModel SubChannel;
+
+        private IModel PubChannel;
+
+        public void Initialize()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            var connection = factory.CreateConnection();
+
+            PubChannel = connection.CreateModel();
+            SubChannel = connection.CreateModel();
+
+
+            SubChannel.QueueDeclare(queue: "Producer",
+                durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            PubChannel.QueueDeclare(queue: "Consumer",
+                durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        }
+        public void Subscribe()
+        {
+            var consumer = new EventingBasicConsumer(SubChannel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                Output.Invoke(message);
+            };
+
+            SubChannel.BasicConsume(queue: "Producer",
+                                    autoAck: true,
+                                    consumer: consumer);
+        }
+
+        public void Send(string message)
+        {
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            PubChannel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "Consumer",
+                                 basicProperties: null,
+                                 body: body);
+        }
+    }
+}
---
+++ RabbitDevProducer/RabbitDevProducer/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using RabbitDevTool.Core;
+using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace RabbitDevTool
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Engine Engine;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Engine = new Engine();
+
+            Engine.Output = new Action<string> (DisplayMessage);
+
+            Engine.Initialize();
+
+            Engine.Receiver();
+        }
+        private void DisplayMessage(string message)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
+            {
+                this.clientTextBox.AppendText($"{message}\r");
+            }));
+        }
+        private void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Engine.Send(this.textMessage.Text);
+        }
+    }
+}
